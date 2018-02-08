d001502e6062eac87f0bafc5c30e4631a4844d2e
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-07 22:01:36 -0600

    Updated config with members page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
             '/': { page: '/' },
             '/about': { page: '/about' },
             '/officers': { page: '/officers' },
+            '/members': { page: '/members' },
             '/events': { page: '/events' },
             '/events/ef-18-aps': { page: '/events/ef-18-aps' }
         }
