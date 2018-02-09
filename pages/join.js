a887d1a86dac8c5a3d88618bcc84f5672a0eff61
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-02-08 22:47:17 -0600

    Slight change to the join us page

--- pages/join.js
+++ pages/join.js
@@ -23,12 +23,12 @@ export default () => (
                             <li className="gold"><a href="https://www.tbp.org/scholarships.cfm" target="_blank">Scholarhips</a></li>
                             <p>Awards of <b>$2,000</b> or <b>$1,000</b> are given to Tau Beta Pi members for full-time engineering study in their senior year.
 Tau Beta Pi awards at least <b>200</b> scholarships each year, thanks to the generosity of our alumni.</p>
-                            <li className="gold"><a href="https://www.tbp.org/fellowships.cfm">Fellowships</a></li>
+                            <li className="gold"><a href="https://www.tbp.org/fellowships.cfm" target="_blank">Fellowships</a></li>
                             <p>
                                 Tau Beta Pi awards at least <b>25</b> fellowships, all of whom receive <b>$10,000</b> cash award for one year of graduate study.
 The program has awarded more than <b>$6,239,000</b> to <b>82</b> groups of fellows.
                             </p>
-                            <li className="gold"><a href="https://www.tbp.org/memb/careers.cfm">Career Assistance</a></li>
+                            <li className="gold"><a href="https://www.tbp.org/memb/careers.cfm" target="_blank">Career Assistance</a></li>
                             <p>
                                 Through corporate sponsorships, Tau Beta Pi makes it possible for you to put your resume in the hands of recruiters from companies such as Amazon, Capital One and more.
                             Our website includes access to an online job board connecting our members with top engineering companies that seek to hire interns, entry-level engineers, and seasoned alumni. Resumes may be posted anonymously.
@@ -44,8 +44,7 @@ The program has awarded more than <b>$6,239,000</b> to <b>82</b> groups of fello
                             <p>
                                 District Conferences bring together students from other chapters and interested alumni.
                             Attending the annual <a href="https://www.tbp.org/convention.cfm" target="_blank">convention</a> is an opportunity for members to have a say in how Tau Beta Pi operates, and meet members and alumni from chapters across the country.
-                            Tau Beta Pi has a LinkedIn group <a href="http://www.linkedin.com/groups?home=&gid=101390&trk=anet_ug_hm" target="_blank"><i className="fa fa-linkedin-square"></i></a>, a Facebook page <a href="https://www.facebook.com/groups/2204574940/about/" target="_blank"><i className="fa fa-facebook-square"></i></a>, a Twitter account <a href="http://www.twitter.com/TauBetaPi" target="_blank"><i className="fa fa-twitter-square"></i></a>, and a blog <a href="http://taubetapiathq.wordpress.com/" target="_blank"><i className="fa fa-wordpress"></i></a>. Learn more here.
-                            Alumni chapters around the country host networking and social events.
+                            Tau Beta Pi has a LinkedIn group <a href="http://www.linkedin.com/groups?home=&gid=101390&trk=anet_ug_hm" target="_blank"><i className="fa fa-linkedin-square"></i></a>, a Facebook page <a href="https://www.facebook.com/groups/2204574940/about/" target="_blank"><i className="fa fa-facebook-square"></i></a>, a Twitter account <a href="http://www.twitter.com/TauBetaPi" target="_blank"><i className="fa fa-twitter-square"></i></a>, and a blog <a href="http://taubetapiathq.wordpress.com/" target="_blank"><i className="fa fa-wordpress"></i></a>.
                             </p>
                             <p>To learn more, visit <a href="https://www.tbp.org/join.cfm" target="_blank">TBP.ORG</a></p>
                         </ol>
