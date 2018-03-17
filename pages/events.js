9e4d510c55585a0cc25cd4ec8d571790bbcdd33f
iliyaML <mohamad.i.mohamad.lokman@vanderbilt.edu>
2018-03-16 20:40:44 -0500

    Event changes

--- pages/events.js
+++ pages/events.js
@@ -17,15 +17,62 @@ export default () => (
                 <div className="row">
                     <div className="col-lg-12">
                         <ul className="timeline">
-                            <li className="timeline-inverted" id="current">
+                            <li className="timeline-inverted">
                                 <div className="timeline-image">
                                     <Link href="/events/ef-18-aps">
-                                        <img className="rounded-circle img-fluid" src="/static/img/events/EF_APS_500.jpg" alt="Engineering Futures - Analytical Problem Solving" />
+                                        <img className="rounded-circle img-fluid" src="https://vu-taubetapi.github.io/static/img/events/Bent.jpg" alt="Bent Polishing" />
                                     </Link>
                                 </div>
                                 <div className="timeline-panel">
                                     <div className="timeline-heading white">
-                                        <h4>February 2018</h4>
+                                        <h4 className="subheading">
+                                            <Link href="/events">
+                                                <a>
+                                                    Bent Polishing
+                                                </a>
+                                            </Link>
+                                        </h4>
+                                    </div>
+                                    <div className="timeline-body">
+                                        <p className="text-muted">
+                                            <b>Date:</b> Saturday, March 17<br />
+                                            <b>Time:</b> 9AM - 10AM<br />
+                                            <b>Venue:</b> FGH Entrance<br />
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li id="current">
+                                <div className="timeline-image">
+                                    <img className="rounded-circle img-fluid" src="https://vu-taubetapi.github.io/static/img/events/PiDay.png" alt="Tau Beta Pi Day" />
+                                </div>
+                                <div className="timeline-panel">
+                                    <div className="timeline-heading white">
+                                        <h4 className="subheading">
+                                            <Link href="/events">
+                                                <a>
+                                                    Tau Beta Pi Day
+                                                </a>
+                                            </Link>
+                                        </h4>
+                                    </div>
+                                    <div className="timeline-body">
+                                        <p className="text-muted">
+                                            <b>Date:</b> Wednesday, March 4<br />
+                                            <b>Time:</b> 11AM - 1PM<br />
+                                            <b>Venue:</b> FGH Atrium<br />
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li className="timeline-inverted">
+                                <div className="timeline-image">
+                                    <Link href="/events/ef-18-aps">
+                                        <img className="rounded-circle img-fluid" src="https://vu-taubetapi.github.io/static/img/events/EF_APS_500.jpg" alt="Engineering Futures - Analytical Problem Solving" />
+                                    </Link>
+                                </div>
+                                <div className="timeline-panel">
+                                    <div className="timeline-heading white">
                                         <h4 className="subheading">
                                             <Link href="/events/ef-18-aps">
                                                 <a>
@@ -38,7 +85,7 @@ export default () => (
                                         <p className="text-muted">
                                             <b>Date:</b> Saturday, February 3<br />
                                             <b>Time:</b> 10AM - 2PM<br />
-                                            <b>Venue:</b> Featheringill Hall 129<br />
+                                            <b>Venue:</b> FGH 129<br />
                                             <b>Registration:</b> <a href="http://goo.gl/u2bket" target="_blank">goo.gl/u2bket</a></p>
                                     </div>
                                 </div>
