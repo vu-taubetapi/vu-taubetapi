c3e36b91678a3599f96781066b5804208e7b1563
mohdsai <irfan.sabri.bin.mohd.sabri@vanderbilt.edu>
2018-11-08 14:03:58 -0600

    updated member list with spring 2018

---
+++ components/spring_2018.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        "name": "Allison Albright",
+        "major": "Chemical Engineering",
+        "class": 2019
+    },
+    {
+        "name": "Xueying Ding",
+        "major": "Computer Science",
+        "class": 2019
+    },
+    {
+        "name": "Liam Kelly",
+        "major": "Electrical Engineering",
+        "class": 2019
+    },
+    {
+        "name": "Ali Yazdani",
+        "major": "Mechanical Engineering",
+        "class": 2019
+    },
+    {
+        "name": "Kristin Quah",
+        "major": "Electrical Engineering",
+        "class": 2019
+    },
+    {
+        "name": "Anthony Swenson",
+        "major": "Engineering Science",
+        "class": 2019
+    },
+    {
+        "name": "Harrison Yan",
+        "major": "Mechanical Engineering",
+        "class": 2019
+    },
+    {
+        "name": "Yifan Chen",
+        "major": "Computer Science",
+        "class": 2019
+    },
+    {
+        "name": "Elizabeth Curvino",
+        "major": "",
+        "class": 2018
+    },
+    {
+        "name": "Matthew Johnson",
+        "major": "",
+        "class": 2018
+    },
+    {
+        "name": "Christopher Khan",
+        "major": "",
+        "class": 2018
+    },
+    {
+        "name": "Alana Suter",
+        "major": "",
+        "class": 2018
+    }
+];
--- pages/members.js
+++ pages/members.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import fall_2017 from '../components/fall_2017'
 import spring_2017 from '../components/spring_2017'
 import fall_2016 from '../components/fall_2016'
+import spring_2018 from '../components/spring_2018'
 
 export default () => (
     <div>
@@ -10,6 +11,26 @@ export default () => (
         <Navbar />
         <section id="members">
             <div className="container">
+                <div className="row">
+                    <div className="col-lg-12 text-center">
+                        <h2 className="section-heading text-uppercase gold">Members</h2>
+                        <h3 className="section-subheading text-muted">Initiated Spring 2018</h3>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    {
+                        spring_2018.map((value, index) => {
+                            return (
+                                <div className="col-sm-4 mb-3" key={index}>
+                                    <div className="text-muted">
+                                        <span className="gold">{value.name}</span><br />
+                                        {value.major} '{value.class.toString().substring(2)}
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
                 <div className="row">
                     <div className="col-lg-12 text-center">
                         <h2 className="section-heading text-uppercase gold">Members</h2>
